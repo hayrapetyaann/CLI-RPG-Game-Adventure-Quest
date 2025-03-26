b01ae28ab998836e6cbc214ec0ef4f4931458ea8
hayrapetyaann <hayrapetyannelenn@gmail.com>
2025-03-26 19:21:00 +0400

    Add header files for game.

--- src/Character.cpp
+++
@@ -1,9 +0,0 @@
-#include "Character.h"
-
-Character::Character(const std::string& name, int health, int attackPower, int defense) 
-        : m_name(name), m_health(health), m_attackPower(attackPower), m_defense(defense) {}
-
-std::string Character::getName() const {
-    return m_name;
-}
-
